52005f489d32a87861b472f0a37cee13025aa84b
Youngho <cyh6099@gmail.com>
2022-11-21 01:35:54 +0900

    feat : 사용자 이동 입력과 해당 단계의 다리를 비교해 이동할 수 있는지 판단하는 기능 구현

--- src/main/java/bridge/Bridge.java
+++ src/main/java/bridge/Bridge.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class Bridge {
     private final List<String> bridge;
 
-    public Bridge(int bridgeSize, BridgeMaker bridgeMaker) {
-        this.bridge = bridgeMaker.makeBridge(bridgeSize);
+    public Bridge(List<String> bridge) {
+        this.bridge = bridge;
+    }
+
+    public boolean isPassStep(int index, String playerMoving) {
+        return bridge.get(index).equals(playerMoving);
     }
 }
