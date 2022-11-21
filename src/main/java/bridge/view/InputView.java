69ab67b285a3da02a5a04850f9f0089cdbe0eab7
Youngho <cyh6099@gmail.com>
2022-11-21 16:35:05 +0900

    feat : 사용자 재시작 여부 입력에 따른 유효성 검사 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -37,7 +37,10 @@ public class InputView {
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     public String readGameCommand() {
-        return null;
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+        String gameCommand = Console.readLine();
+        InputValidator.validateGameCommandInput(gameCommand);
+        return gameCommand;
     }
 
 
