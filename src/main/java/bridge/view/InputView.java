7a5908cecd196a3ad5e847df9532cbce3798b394
Youngho <cyh6099@gmail.com>
2022-11-21 14:23:23 +0900

    feat : 유효한 플레이어 이동 입력이 들어올 때 까지 입력을 반복해서 받는 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -23,7 +23,14 @@ public class InputView {
      * 사용자가 이동할 칸을 입력받는다.
      */
     public String readMoving() {
-        return null;
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+        String playerMoving = Console.readLine();
+        try {
+            InputValidator.validatePlayerMovingInput(playerMoving);
+        } catch (IllegalArgumentException exception) {
+            readMoving();
+        }
+        return playerMoving;
     }
 
     /**
