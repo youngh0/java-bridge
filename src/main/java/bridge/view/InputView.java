8450dd129dd607692c1861f467e4d8d7098630f8
Youngho <cyh6099@gmail.com>
2022-11-22 00:41:54 +0900

    fix : 사용자로부터 입력받는 기능 구현 방식 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -3,45 +3,44 @@ package bridge.view;
 import bridge.utils.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 public class InputView {
-
     public int readBridgeSize() {
-        String bridgeSize = Console.readLine();
-        try {
-            InputValidator.validateBridgeSizeInput(bridgeSize);
-        } catch (IllegalArgumentException exception) {
-            System.out.println("error");
-            readBridgeSize();
+        while (true) {
+            try {
+                System.out.println("다리의 길이를 입력해주세요. ");
+                String bridgeSize = Console.readLine();
+                InputValidator.validateBridgeSizeInput(bridgeSize);
+                return Integer.parseInt(bridgeSize);
+            } catch (IllegalArgumentException exception) {
+                System.out.println("error");
+            }
         }
-        return Integer.parseInt(bridgeSize);
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     public String readMoving() {
-        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
-        String playerMoving = Console.readLine();
-        try {
-            InputValidator.validatePlayerMovingInput(playerMoving);
-        } catch (IllegalArgumentException exception) {
-            readMoving();
+        while (true) {
+            try {
+                System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+                String playerMoving = Console.readLine();
+                InputValidator.validatePlayerMovingInput(playerMoving);
+                return playerMoving;
+            } catch (IllegalArgumentException exception) {
+                System.out.println("[ERROR]");
+            }
         }
-        return playerMoving;
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     public String readGameCommand() {
-        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
-        String gameCommand = Console.readLine();
-        InputValidator.validateGameCommandInput(gameCommand);
-        return gameCommand;
+        while (true) {
+            try {
+                System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+                String gameCommand = Console.readLine();
+                InputValidator.validateGameCommandInput(gameCommand);
+                return gameCommand;
+            } catch (IllegalArgumentException exception) {
+                System.out.println("[ERROR]");
+            }
+        }
+
     }
-
-
 }
