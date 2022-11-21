6e0d25e33a769f50d5e5f2622c8510646ad8882d
Youngho <cyh6099@gmail.com>
2022-11-21 14:10:17 +0900

    test : 현재까지 진행중인 게임 결과 확인하는 테스트 작성

---
+++ src/test/java/bridge/BridgeGameResultTest.java
@@ -0,0 +1,23 @@
+package bridge;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.util.Lists.newArrayList;
+
+class BridgeGameResultTest {
+
+    @Test
+    void getCurrentResultTest() {
+        BridgeNumberGenerator numberGenerator = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0, 1));
+        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
+        Bridge bridge = new Bridge(bridgeMaker.makeBridge(4));
+        BridgeGameResult bridgeGameResult = new BridgeGameResult(bridge.getBridgeSize());
+
+        new BridgeGame(bridge, bridgeGameResult).move(0, "U");
+        StringBuffer currentResult = bridgeGameResult.getCurrentResult(1);
+        String answer = "[ O ]\n" + "[   ]\n";
+
+        Assertions.assertThat(currentResult.toString()).isEqualTo(answer);
+    }
+}
