a402b4da3d369e0c319340344c0d04e8a9860919
Youngho <cyh6099@gmail.com>
2022-11-22 12:14:21 +0900

    refactor : 테스트 코드 하드코딩 값 상수로 변경

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -1,15 +1,14 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
-
-import camp.nextstep.edu.missionutils.test.NsTest;
-
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
 
