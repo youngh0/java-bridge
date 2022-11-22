f1e89cbec6638f632afff9609594485d291b4a71
Youngho <cyh6099@gmail.com>
2022-11-22 11:29:16 +0900

    fix : bridge테스트 로직 간단하게 수정

--- src/test/java/bridge/model/BridgeTest.java
+++ src/test/java/bridge/model/BridgeTest.java
@@ -1,13 +1,12 @@
-package bridge;
+package bridge.model;
 
-import bridge.model.Bridge;
-import bridge.utils.BridgeNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.util.Lists.newArrayList;
 
 @DisplayName("각 스템 별로 사용자 입력이 해당 다리 칸을 건널 수 있는지")
 class BridgeTest {
@@ -15,9 +14,7 @@ class BridgeTest {
     @CsvSource(value = {"0:U", "1:D", "2:D", "3:U"}, delimiter = ':')
     @DisplayName("입력과 다리의 해당 칸 값이 일치한다면 true")
     void isPassStepTest(int index, String moving) {
-        BridgeNumberGenerator numberGenerator = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0, 1));
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        Bridge bridge = new Bridge(bridgeMaker.makeBridge(4));
+        Bridge bridge = new Bridge(List.of("U", "D", "D", "U"));
         assertThat(bridge.isPassStep(index, moving)).isEqualTo(true);
     }
 
@@ -25,9 +22,7 @@ class BridgeTest {
     @CsvSource(value = {"0:D", "1:U", "2:U", "3:D"}, delimiter = ':')
     @DisplayName("입력과 다리의 해당 칸 값이 일치하지 않으면 false")
     void isPassStepFalseTest(int index, String moving) {
-        BridgeNumberGenerator numberGenerator = new ApplicationTest.TestNumberGenerator(newArrayList(1, 0, 0, 1));
-        BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        Bridge bridge = new Bridge(bridgeMaker.makeBridge(4));
+        Bridge bridge = new Bridge(List.of("U", "D", "D", "U"));
         assertThat(bridge.isPassStep(index, moving)).isEqualTo(false);
     }
 }
