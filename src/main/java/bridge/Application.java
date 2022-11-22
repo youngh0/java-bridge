1fcf794f53395052c1fd697c76c3a36aab5aa319
Youngho <cyh6099@gmail.com>
2022-11-22 11:34:46 +0900

    refactor : 개행 제거

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -7,7 +7,6 @@ import bridge.model.BridgeGame;
 import bridge.model.BridgeGameResult;
 
 public class Application {
-
     public static void main(String[] args) {
         GameSettingController gameSettingController = new GameSettingController();
         Bridge bridge = gameSettingController.generateBridge(new BridgeRandomNumberGenerator());
