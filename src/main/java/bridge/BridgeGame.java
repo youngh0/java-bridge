7e0ae19403a05c7bdd8d1df1228bbf65e297580f
Youngho <cyh6099@gmail.com>
2022-12-16 22:51:18 +0900

    feat : 재시도 시 게임시도 횟수 추가, 게임 결과 초기화 구현

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -10,7 +10,8 @@ import java.util.List;
  * 다리 건너기 게임을 관리하는 클래스
  */
 public class BridgeGame {
-    private final GameResult gameResult;
+    private GameResult gameResult;
+    private int tryCount = 1;
 
     public BridgeGame() {
         this.gameResult = new GameResult();
@@ -28,11 +29,17 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+        this.gameResult = new GameResult();
+        tryCount++;
     }
 
     public Bridge makeBridge(BridgeLength bridgeLength) {
         BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
         List<String> bridge = bridgeMaker.makeBridge(bridgeLength.getBridgeLength());
         return new Bridge(bridge);
+    }
+
+    public int getTryCount() {
+        return tryCount;
     }
 }
