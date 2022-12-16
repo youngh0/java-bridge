4760c529ee2ba525e4f6f0cc0062a18802c57fb4
Youngho <cyh6099@gmail.com>
2022-12-16 22:50:42 +0900

    test : 재시도 시 게임시도 횟수 추가, 게임 결과 초기화 테스트

--- src/test/java/bridge/BridgeGameTest.java
+++ src/test/java/bridge/BridgeGameTest.java
@@ -2,6 +2,7 @@ package bridge;
 
 import bridge.domain.Bridge;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,11 +10,20 @@ import java.util.List;
 
 class BridgeGameTest {
     Bridge bridge = new Bridge(List.of("U", "D", "D", "U"));
+    BridgeGame bridgeGame = new BridgeGame();
 
     @ParameterizedTest
     @CsvSource(value = {"U,true", "D,false"})
     void 사용자_입력으로_정답_여부_테스트(String playerMoving, boolean answer) {
-        BridgeGame bridgeGame = new BridgeGame();
+
         Assertions.assertThat(bridgeGame.move(playerMoving, bridge)).isEqualTo(answer);
     }
+
+    @Test
+    void 재시작시_게임시도횟수_변경_테스트() {
+        bridgeGame.retry();
+        Assertions.assertThat(bridgeGame.getTryCount()).isEqualTo(2);
+    }
+
+
 }
--- src/test/java/bridge/domain/GameResultTest.java
+++ src/test/java/bridge/domain/GameResultTest.java
@@ -1,14 +1,27 @@
 package bridge.domain;
 
+import bridge.BridgeGame;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class GameResultTest {
+    BridgeGame bridgeGame = new BridgeGame();
+    GameResult gameResult = new GameResult();
+    Bridge bridge = new Bridge(List.of("U", "D", "D", "U"));
 
     @Test
     void updateResult() {
         GameResult gameResult = new GameResult();
         gameResult.updateResult("U", true);
         Assertions.assertThat(gameResult.getCurrentStep()).isEqualTo(1);
+    }
+
+    @Test
+    void 재시작시_게임결과_초기화_테스트() {
+        bridgeGame.move("U", bridge);
+        bridgeGame.retry();
+        Assertions.assertThat(gameResult.getCurrentStep()).isEqualTo(0);
     }
 }
